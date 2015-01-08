ebd1f5a3dae22b269716eca29b42efdf3eac9213
zaekeon <zaekeon@gmail.com>
2015-01-08 13:39:33 -0600

    Commented code, added while example

--- src/IfShorthand.java
+++ src/IfShorthand.java
@@ -4,6 +4,8 @@ public class IfShorthand {
 	public static void main(String[] args)
 	{
 		String test = "hello";
+	
+	//Using the shorthand way of doing an if..else with the ? conditional operator.
 		
 	System.out.println(test == "hello" ? "True" : "False");
 	}
---
+++ src/WhileExample.java
@@ -0,0 +1,18 @@
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+	
+		
+		int chickens = 3;
+		
+		while (chickens <=100)
+		{
+			chickens = 3 * chickens;
+			System.out.println("The value of chickens is: " + chickens);
+			
+		}
+
+	}
+
+}
