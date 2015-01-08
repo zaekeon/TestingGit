e61b7a5f82f48f855367a1ad6ea78f954ee39f97
zaekeon <zaekeon@gmail.com>
2015-01-08 13:22:11 -0600

    Created example if class

---
+++ src/IfExample.java
@@ -0,0 +1,19 @@
+
+public class IfExample {
+
+	
+public static void main(String[] args)
+{
+	String test = "hello";
+	
+	if (test == "hello")
+	{
+		System.out.println("The test returned true");
+		
+	}
+	else
+	{
+		System.out.println("The test returned false");
+	}
+}
+}
