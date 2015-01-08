66a25a1dd1d0c04f7b7f08e252c1825d417aad7c
zaekeon <zaekeon@gmail.com>
2015-01-08 13:54:58 -0600

    added class average example class

---
+++ src/ClassAverage.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class ClassAverage {
+
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int total = 0;
+		int gradeCounter = 1;
+		
+		while (gradeCounter <= 10)
+		{
+			System.out.print("Enter grade: ");
+			int grade = input.nextInt();
+			total = total + grade;
+			gradeCounter++;
+			
+		}
+		
+		int average = total / 10;
+		
+		System.out.printf("%nTotal of all 10 grades is %d%n", total);
+		System.out.printf("Class average is %d%n", average);
+		
+		
+	}
+}
