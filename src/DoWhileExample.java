03d281faf0fd5ea532e303e80ac690cf713df91d
zaekeon <zaekeon@gmail.com>
2015-01-09 08:35:04 -0600

    Added do while example class

---
+++ src/DoWhileExample.java
@@ -0,0 +1,15 @@
+		
+public class DoWhileExample {
+
+	
+	public static void main(String[] args)
+	{
+		int counter =1;
+		
+		do
+		{
+			System.out.println("hello" + counter);
+			++counter;
+		} while (counter <=10);
+	}
+}
