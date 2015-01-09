4ed88e89f2d0e5f8b5d8aa459911e8c8ad3fdec8
zaekeon <zaekeon@gmail.com>
2015-01-09 09:20:45 -0600

    added switch string exmaple class

---
+++ src/SwitchStringExample.java
@@ -0,0 +1,22 @@
+
+public class SwitchStringExample {
+
+	
+	
+	
+	public static void main(String[] args)
+	{
+	String testString = "yes";
+	switch (testString)
+	{
+	case "yes":
+	System.out.println("value was yes");
+	break;
+	case "no":
+	System.out.println("value was no");
+	break;
+	} //end switch
+	
+	
+	} //end main
+} //end class
