e11297014a6786a0e390fc4c73351790b5e8c381
zaekeon <zaekeon@gmail.com>
2015-01-09 08:04:05 -0600

    Added WhileCounter class

---
+++ src/WhileCounter.java
@@ -0,0 +1,14 @@
+		
+public class WhileCounter {
+
+	public static void main(String[] args)
+	{
+		int counter = 0;
+		while(counter <= 10	)
+		{
+		System.out.println(counter);
+		
+		counter++; //increment counter
+		}
+	}
+}
