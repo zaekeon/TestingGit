232e25bd5343d50145779ae9f92c3386943b84b5
zaekeon <zaekeon@gmail.com>
2015-01-09 12:36:45 -0600

    Added shapestest and shapes class

---
+++ src/ShapesTest.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class ShapesTest {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		String input = JOptionPane.showInputDialog("Enter 1 to draw rectangles\n" + "Enter 2 to draw ovals");
+		
+		int choice = Integer.parseInt(input);
+		Shapes testShape = new Shapes(choice);
+		
+		JFrame application = new JFrame();
+		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(testShape);
+		application.setSize(300, 300);
+		application.setVisible(true);
+		
+
+	}
+
+}
