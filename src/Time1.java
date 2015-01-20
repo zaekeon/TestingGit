2d54f3b6a4ebb5b63f1645737c7e6bf59232931a
zaekeon <zaekeon@gmail.com>
2015-01-20 13:37:36 -0600

    Added time and timetest class

---
+++ src/Time1.java
@@ -0,0 +1,35 @@
+
+public class Time1 {
+private int hour;
+private int minute;
+private int second;
+
+
+public void setTime(int hour, int minute, int second)
+{
+	if (hour < 0 || hour >= 24 || minute < 0 || minute >= 60 || second < 0 || second >= 60)
+		{
+		throw new IllegalArgumentException("hour, minute and/or second was out of range");
+		}
+	
+	this.hour = hour;
+	this.minute = minute;
+	this.second = second;
+	
+	
+		}
+
+//convert string in universal-time format
+
+public String toUniversalString()
+{
+	return String.format("_d:_d:_d:",hour,minute, second);
+	
+}
+
+public String toString()
+{
+	return String.format("%d:_d:_d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12), minute, second, (hour < 12 ? "AM" : "PM"));
+	
+}
+}
