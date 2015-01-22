b7d118b146333ae773cfcb2d69b63f9d2cf25809
zaekeon <zaekeon@gmail.com>
2015-01-22 14:05:31 -0600

    Added employee test class for composition

---
+++ src/EmployeeTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created by john.whites on 1/22/2015.
+ */
+public class EmployeeTest {
+
+    public static void main(String[] args)
+    {
+        Date birth = new Date(7,24,1949);
+        Date hire = new Date(3,12,1988);
+        Employee employee = new Employee("Bob", "Blue", birth, hire);
+
+        System.out.println(employee);
+    }
+}
