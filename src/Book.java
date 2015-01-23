388344cb5c53240b6a80adadf9f8057a7f9a267a
zaekeon <zaekeon@gmail.com>
2015-01-23 08:28:11 -0600

    Added enum and book classes

---
+++ src/Book.java
@@ -0,0 +1,32 @@
+
+public enum Book {
+    //declare constants of enum type
+    JHTP("Java How to  Program", "2015"),
+    CHTP("C How to Program", "2013"),
+    IW3HTP("Internet & World Wide Web How to Program","2012"),
+    CPPHTP("C++ How to Program", "2014"),
+    CSHARPHTP("Visual C# How to Program", "2014");
+
+    //instance fields
+    private final String title;
+    private final String copyrightYear;
+
+    //enum constructor
+    Book(String title, String copyrightYear)
+    {
+        this.title = title;
+        this.copyrightYear = copyrightYear;
+
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getCopyrightYear()
+    {
+        return copyrightYear;
+    }
+
+}
