a864b459eebca3ddf17bb2fffe32599664e1c864
zaekeon <zaekeon@gmail.com>
2015-02-02 12:11:35 -0600

    added basepluscommissionemployee2 class

---
+++ src/BasePlusCommissionEmployee2.java
@@ -0,0 +1,56 @@
+
+public class BasePlusCommissionEmployee2 extends CommissionEmployee2 {
+
+	private double baseSalary;
+	
+	
+	
+	public BasePlusCommissionEmployee2(String firstName, String lastName,
+			String socialSecurityNumber, double grossSales,
+			double commissionRate, double baseSalary) {
+		super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+
+		
+	if (baseSalary < 0.0) //validate base salary
+	{
+		throw new IllegalArgumentException("Base Salary must be >= 0.0");
+		
+	}
+	
+	this.baseSalary =  baseSalary;
+		
+		
+		
+	}
+	
+	public void setBaseSalary(double baseSalary)
+	{
+		if (baseSalary < 0.0) //validate baseSalary
+			throw new IllegalArghumentException("Base salary must be >= 0.0");
+		
+		this.baseSalary = baseSalary;
+	}
+	
+	public double getBaseSalary()
+	{
+		return baseSalary;
+	}
+	
+	@Override
+	public double earnings()
+	{
+		return getBaseSalary() + super.earnings();
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("%s %s; %s: $%,.2f","base-salaried", super.toString(), "base salary", getBaseSalary());
+	}
+	
+	
+	
+
+	
+	
+}
