6d6e9a18e0ae7d311d4e36bf7b4d78cb98e2d5f1
unknown <zaekeon@gmail.com>
2015-02-21 10:05:34 -0600

    added karilyn class

---
+++ src/Karilyn.java
@@ -0,0 +1,22 @@
+
+public class Karilyn {
+
+	boolean isPregnant = false;
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Blah blah blah blah!");
+		System.out.println("yell yell yell yell");
+		System.out.println("I'm always right");
+		
+		int [] meow = {3,5,6,7};
+		
+		for (int i : meow)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
